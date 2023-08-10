6dfd6f2029cbc8479af6e0c61727ff5a739ce5da
daniele <daniele.russo.vs@gmail.com>
2023-08-10 17:08:18 +0200

    Final implementation

--- LychrelNumbers/src/lychrel/Lychrel.java
+++ LychrelNumbers/src/lychrel/Lychrel.java
@@ -1,14 +1,38 @@
 package lychrel;
 
+import java.math.BigInteger;
+
 public class Lychrel {
     public static int convergesAtIteration(int number, int limit) {
-        if (isPalindrome(number)) return 0;
-        else return 1;
+        return converge(BigInteger.valueOf(number), 0, limit);
     }
 
-    private static boolean isPalindrome(int number) {
-        if(number <= 9) return true;
+    private static int converge(BigInteger number, int iteration, int limit) {
+        if (!isPalindrome(number) && iteration < limit)
+            return converge(number.add(reverse(number)), iteration + 1, limit);
+        else
+            return iteration;
+    }
 
-        return false;
+    static BigInteger reverse(BigInteger number) {
+        char[] nDigits = number.toString().toCharArray();
+        char[] rDigits = new char[nDigits.length];
+        int lastIndex = nDigits.length - 1;
+
+        for (int idx = 0; idx < nDigits.length; idx++)
+            rDigits[idx] = nDigits[lastIndex - idx];
+
+        return new BigInteger(new String(rDigits));
+    }
+
+    static boolean isPalindrome(BigInteger number) {
+        String digits = number.toString();
+        int lastIndex = digits.length() - 1;
+
+        for (int idx = 0; idx < digits.length(); idx++)
+            if (digits.charAt(idx) != digits.charAt(lastIndex - idx))
+                return false;
+
+        return true;
     }
 }
--- LychrelNumbers/src/lychrel/LychrelTest.java
+++ LychrelNumbers/src/lychrel/LychrelTest.java
@@ -1,8 +1,11 @@
 package lychrel;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.math.BigInteger;
+
+import static org.junit.Assert.*;
 
 public class LychrelTest {
 
@@ -14,9 +17,55 @@ public class LychrelTest {
         convergesAtIteration(2, 0);
         convergesAtIteration(10, 1);
         convergesAtIteration(11, 0);
+        convergesAtIteration(19, 2);
+        convergesAtIteration(78, 4);
+        convergesAtIteration(89, 24);
+
+        doesNotConverge(196);
+    }
+
+    private void doesNotConverge(int number) {
+        convergesAtIteration(number, LIMIT);
     }
 
     private void convergesAtIteration(int number, int iteration) {
         assertEquals(iteration, Lychrel.convergesAtIteration(number, LIMIT));
+    }
+
+    @Test
+    public void palindromes(){
+        isPalindrome(1);
+        isPalindrome(11);
+        isPalindrome(121);
+        isPalindrome(12321);
+        isPalindrome(1234321);
+    }
+
+    @Test
+    public void notPalindromes(){
+        isNotPalindrome(10);
+        isNotPalindrome(12331);
+        isNotPalindrome(124321);
+    }
+
+    private void isNotPalindrome(int number) {
+        assertFalse(Lychrel.isPalindrome(BigInteger.valueOf(number)));
+    }
+
+    private void isPalindrome(int number){
+        assertTrue(Lychrel.isPalindrome(BigInteger.valueOf(number)));
+    }
+
+    @Test
+    public void reversals(){
+        reversed(1,1);
+        reversed(12, 21);
+        reversed(13, 31);
+        reversed(123, 321);
+        reversed(1234, 4321);
+    }
+
+    private void reversed(int n, int r) {
+        assertEquals(BigInteger.valueOf(r), Lychrel.reverse(BigInteger.valueOf(n)));
     }
 }
